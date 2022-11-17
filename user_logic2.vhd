1e54d283c390b7d14f506b0d1e06461e47f8fc3a
kaynegalie <115092041+kaynegalie@users.noreply.github.com>
2022-11-16 20:21:11 -0500

    Update user_logic2.vhd

--- user_logic2.vhd
+++ user_logic2.vhd
@@ -15,3 +15,29 @@ entity user_logic2 is
     yx_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
   );
 end user_logic2;
+ 
+ architecture Behavioral of user_logic2 is
+COMPONENT cordic_0
+  PORT (
+    aclk : IN STD_LOGIC;
+    s_axis_cartesian_tvalid : IN STD_LOGIC;
+    s_axis_cartesian_tdata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    s_axis_phase_tvalid : IN STD_LOGIC;
+    s_axis_phase_tdata : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+    m_axis_dout_tvalid : OUT STD_LOGIC;
+    m_axis_dout_tdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+  );
+END COMPONENT;
+begin
+U : div_gen_0
+  PORT MAP (
+    aclk => ck,
+    s_axis_cartesian_tvalid => in_tvalid,
+    s_axis_cartesian_tdata => y,
+    s_axis_phase_tvalid => in_tvalid,
+    s_axis_phase_tdata => x,
+    m_axis_dout_tvalid => out_tvalid,
+    m_axis_dout_tdata => yx_out
+  );
+ 
+end Behavioral;
